b21ca0aacdb8b8f95faeb0f06b156384255b9b9c
nemota <walkinggreencoordinator@gmail.com>
2020-08-30 15:25:54 +0900

    Finished.

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-//Synchtube Premium、jQueryに依存します
-var ele;
+//Synchtube Premiumに依存します
 (function () {
 	//
 	//要素の追加
@@ -21,6 +20,7 @@ var ele;
  	$chatSaveWrap.innerHTML=
  	'<div id="chatSave-well" class="well from-holizontal">'+
 	'	<div style="text-align: center;">'+
+	'		ダウンロード時のファイル名(拡張子不要)<input type="text" id=file-name class="form-control cs-textbox"><br>'+
 	'		<button class="btn btn-sm btn-default" id="chatSave-startbtn">記録スタート</button><br>'+
 	'		<span id="chatSave-status">スタートボタンを押すと記録を開始します</span>'+
 	'	</div>'+
@@ -51,6 +51,7 @@ var ele;
  	var $messagebuffer=document.getElementById("messagebuffer");
  	var $chatSaveStartBtn=document.getElementById("chatSave-startbtn");
  	var $chatSaveStatus=document.getElementById("chatSave-status");
+ 	var $filename=document.getElementById("file-name");
  	var $lastrecorded;
  	var chatlog="";//出力する文字列
  	
@@ -69,6 +70,10 @@ var ele;
  	$chatSaveStartBtn.onclick=function () {
  		if(!recording){//始めるときなら
  			//始める前の確認
+ 			if($filename.value.trim()==""){
+ 				alert("ファイル名を指定してください");
+ 				return;
+ 			}
  			if (!confirm("記録を開始します。よろしいですか？")){
  				return;
  			}
@@ -77,6 +82,7 @@ var ele;
  			$chatSaveStatus.innerText="準備中";
  			
  			//ボタン操作を一時的に無効に
+ 			$filename.disabled=true;
  			$chatSaveStartBtn.disabled=true;
  			
  			//開始メッセージを表示
@@ -101,13 +107,13 @@ var ele;
 					clearInterval(saveroutine);				
 				}
  				saveRestMessages();
- 			},1000);
+ 			},30000);
  			
  		}else{//終わるときなら
  			if(!confirm("記録を終了してよろしいですか？\n(記録終了後ログファイルがダウンロードされます)")){
  				return;
  			}
- 			$chatSaveStatus.innerText="終了待機中";
+ 			$chatSaveStatus.innerText="終了待機中(30秒以内に終了します)";
  			$chatSaveStartBtn.disabled=true;
 	 		
  			//終了メッセージを表示
@@ -126,7 +132,6 @@ var ele;
  	
  	//まだ保存していないメッセージを保存する
  	function saveRestMessages() {
- 		console.log(chatlog);
  		while($lastrecorded.nextElementSibling){//次の要素がある間
  			$lastrecorded=$lastrecorded.nextElementSibling;
  			
@@ -142,11 +147,11 @@ var ele;
  						for(var tempNode2=tempNode.firstChild;tempNode2!=null;tempNode2=tempNode2.nextSibling){
  							if(tempNode2.getElementsByClassName&&tempNode2.getElementsByClassName("embedimg").length>0){//埋め込み画像があれば
  								//リンクを追加
- 								tempText+=tempNode2.getElementsByClassName("embedimg")[0].src+" ";
+ 								tempText+=tempNode2.getElementsByClassName("embedimg")[0].src;
  								
  							}else if(tempNode2.classList&&tempNode2.classList.contains("channel-emote")){//エモートであれば
  								//エモートタイトルを追加
- 								tempText+=tempNode2.title+" ";
+ 								tempText+=tempNode2.title;
  								
  							}else{//その他のとき
  								//文を追加
@@ -158,11 +163,11 @@ var ele;
  					}else{
  						if(tempNode.getElementsByClassName("embedimg").length>0){//埋め込み画像があれば
  							//リンクを追加
- 							tempText+=tempNode.getElementsByClassName("embedimg")[0].src+" ";
+ 							tempText+=tempNode.getElementsByClassName("embedimg")[0].src;
  							
  						}else if(tempNode.getElementsByClassName("channel-emote").length>0){//エモートがあれば
  							//エモートタイトルを追加
- 							tempText+=tempNode.getElementsByClassName("channel-emote")[0].title+" ";
+ 							tempText+=tempNode.getElementsByClassName("channel-emote")[0].title;
  							
  						}else{//その他のとき
  							//文を追加
@@ -183,9 +188,15 @@ var ele;
  				addChatNotification("記録を終了しました");
  				
  				//blobを作成し、ダウンロード
- 				
+ 				var link=document.createElement("a");
+ 				link.download=$filename.value;
+ 				var blob=new Blob([chatlog],{type:'text/plain'});
+ 				link.href=URL.createObjectURL(blob);
+ 				link.click();
+ 				URL.revokeObjectURL(link.href);
  				
  				//ファイル変更等の操作を有効に
+ 				$filename.disabled=false;
  				$chatSaveStartBtn.disabled=false;
  				
  				//ログをリセット
